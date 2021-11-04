3034b9eb18166557bc90910c5ae095c158155d90
juliocesar757 <julioguimaraes@gmail.com>
2021-11-04 15:36:52 -0300

    Início desenvolvimento das aplicações cliente

---
+++ DesktopClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+using DesktopClient.ServicoEstoque;
+
+
+namespace DesktopClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a proxy object and connect to the service
+            ServicoEstoqueClient proxy = new ServicoEstoqueClient("BasicHttpBinding_IProductsService");
+
+        }
+    }
+}
