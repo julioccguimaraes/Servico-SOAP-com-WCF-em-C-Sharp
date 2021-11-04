d9fcdc776222bc944c0c514f0fd4d246703c5b29
juliocesar757 <julioguimaraes@gmail.com>
2021-11-04 13:01:38 -0300

    Adicionado a biblioteca do serviço

---
+++ EstoqueLibrary/IServicoEstoque.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace EstoqueLibrary
+{
+    // Data contract describing the details of a product passed to client applications
+    [DataContract]
+    public class Produto
+    {
+        [DataMember]
+        private string numeroProduto;
+
+        [DataMember]
+        private string nomeProduto;
+
+        [DataMember]
+        private string descricaoProduto;
+
+        [DataMember]
+        private int estoqueProduto;
+
+        public string NumeroProduto { get => numeroProduto; set => numeroProduto = value; }
+        public string NomeProduto { get => nomeProduto; set => nomeProduto = value; }
+        public string DescricaoProduto { get => descricaoProduto; set => descricaoProduto = value; }
+        public int EstoqueProduto { get => estoqueProduto; set => estoqueProduto = value; }
+    }
+
+    // Service contract describing the operations provided by the WCF service
+    [ServiceContract(Namespace = "http://projetoavaliativo.dm113/01", Name = "IServicoEstoque")]
+    public interface IServicoEstoque
+    {
+        // Get all products
+        [OperationContract]
+        List<string> ListarProdutos();
+
+        // Add a product
+        [OperationContract]
+        bool IncluirProduto(Produto produto);
+
+        // Delete a product
+        [OperationContract]
+        bool RemoverProduto(string NumeroProduto);
+
+        // Get the current stock for a product
+        [OperationContract]
+        int ConsultarEstoque(string NumeroProduto);
+
+        // Add stock for a product
+        [OperationContract]
+        bool AdicionarEstoque(string NumeroProduto, int Quantidade);
+
+        // Subtract stock for a product
+        [OperationContract]
+        bool RemoverEstoque(string NumeroProduto, int Quantidade);
+
+        // Get the details of a single product
+        [OperationContract]
+        Produto VerProduto(string NumeroProduto);
+    }
+
+    [ServiceContract(Namespace = "http://projetoavaliativo.dm113/02", Name = "IServicoEstoqueV2")]
+    public interface IServicoEstoqueV2
+    {
+        // Add stock for a product
+        [OperationContract]
+        bool AdicionarEstoque(string NumeroProduto, int EstoqueProduto);
+
+        // Subtract stock for a product
+        [OperationContract]
+        bool RemoverEstoque(string NumeroProduto, int EstoqueProduto);
+
+        // Get the current stock for a product
+        [OperationContract]
+        int ConsultarEstoque(string NumeroProduto);
+    }
+}
---
+++ EstoqueLibrary/ServicoEstoque.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using EstoqueEntityModel;
+using System.ServiceModel.Activation;
+
+namespace EstoqueLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    // This implementation performs minimal error checking and exception handling
+    [AspNetCompatibilityRequirements(
+    RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ServicoEstoque : IServicoEstoque, IServicoEstoqueV2
+    {
+        public bool AdicionarEstoque(string NumeroProduto, int Quantidade)
+        {
+            try
+            {
+                // Connect to the ProvedorEstoque database
+                using (ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    if (!ProductExists(NumeroProduto, database))
+                        return false;
+                    else
+                    {
+                        // Find the product object that matches the parameters passed
+                        // in to the operation
+                        ProdutoEstoque produto = database.ProdutoEstoque.First(p => string.Compare(p.NumeroProduto, NumeroProduto) == 0);
+
+                        produto.EstoqueProduto += Quantidade;
+                        database.ProdutoEstoque.Add(produto);
+
+                        // Save the change back to the database
+                        database.SaveChanges();
+                    }
+                }
+            }
+            catch
+            {
+                // If an exception occurs, return false to indicate failure
+                return false;
+            }
+            // Return true to indicate success
+            return true;
+        }
+
+        public int ConsultarEstoque(string NumeroProduto)
+        {
+            int quantityTotal = 0;
+            try
+            {
+                // Connect to the ProvedorEstoque database
+                using (ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    if (ProductExists(NumeroProduto, database))
+                    {
+                        // Calculate the sum of all quantities for the specified product
+                        quantityTotal = (from p in database.ProdutoEstoque 
+                                         where string.Compare(p.NumeroProduto, NumeroProduto) == 0
+                                         select p.EstoqueProduto).First();
+                    }
+                }
+            }
+            catch
+            {
+                // Ignore exceptions in this implementation
+            }
+            // Return the stock level
+            return quantityTotal;
+        }
+
+        public bool IncluirProduto(Produto produto)
+        {
+            try
+            {
+                // Connect to the ProductsModel database
+                using (ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    if (ProductExists(produto.NumeroProduto, database))
+                        return false;
+                    else
+                    {
+                        // Add the product
+                        ProdutoEstoque produtoEstoque = new ProdutoEstoque
+                        {
+                            NumeroProduto = produto.NumeroProduto,
+                            NomeProduto = produto.NomeProduto,
+                            DescricaoProduto = produto.DescricaoProduto,
+                            EstoqueProduto = produto.EstoqueProduto
+                        };
+
+                        database.ProdutoEstoque.Add(produtoEstoque);
+
+                        // Save the change back to the database
+                        database.SaveChanges();
+                    }
+                }
+            }
+            catch
+            {
+                // If an exception occurs, return false to indicate failure
+                return false;
+            }
+            // Return true to indicate success
+            return true;
+        }
+
+        public List<string> ListarProdutos()
+        {
+            // Create a list of products
+            List<string> productsList = new List<string>();
+            try
+            {
+                // Connect to the ProductsModel database
+                using (ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    // Fetch the products in the database
+                    List<ProdutoEstoque> products = (from product in database.ProdutoEstoque
+                                              select product).ToList();
+
+                    foreach (ProdutoEstoque product in products)
+                    {
+                        productsList.Add("Número: " + product.NumeroProduto + ", Nome: " + product.NomeProduto + ", Estoque: " + product.EstoqueProduto);
+                    }
+                }
+            }
+            catch
+            {
+                // Ignore exceptions in this implementation
+            }
+            // Return the list of products
+            return productsList;
+        }
+
+        public bool RemoverEstoque(string NumeroProduto, int Quantidade)
+        {
+            try
+            {
+                // Connect to the ProvedorEstoque database
+                using (ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    if (!ProductExists(NumeroProduto, database))
+                        return false;
+                    else
+                    {
+                        // Find the product object that matches the parameters passed
+                        // in to the operation
+                        ProdutoEstoque produto = database.ProdutoEstoque.First(p => string.Compare(p.NumeroProduto, NumeroProduto) == 0);
+
+                        produto.EstoqueProduto -= Quantidade;
+                        database.ProdutoEstoque.Add(produto);
+
+                        // Save the change back to the database
+                        database.SaveChanges();
+                    }
+                }
+            }
+            catch
+            {
+                // If an exception occurs, return false to indicate failure
+                return false;
+            }
+            // Return true to indicate success
+            return true;
+        }
+
+        public bool RemoverProduto(string NumeroProduto)
+        {
+            try
+            {
+                // Connect to the ProvedorEstoque database
+                using (ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    if (!ProductExists(NumeroProduto, database))
+                        return false;
+                    else
+                    {
+                        // Find the product object that matches the parameters passed
+                        // in to the operation
+                        ProdutoEstoque produto = database.ProdutoEstoque.First(p => string.Compare(p.NumeroProduto, NumeroProduto) == 0);
+
+                        database.ProdutoEstoque.Remove(produto);
+
+                        // Save the change back to the database
+                        database.SaveChanges();
+                    }
+                }
+            }
+            catch
+            {
+                // If an exception occurs, return false to indicate failure
+                return false;
+            }
+            // Return true to indicate success
+            return true;
+        }
+
+        public Produto VerProduto(string NumeroProduto)
+        {
+            Produto produto = null;
+            try
+            {
+                // Connect to the ProvedorEstoque database
+                using (ProvedorEstoque database = new ProvedorEstoque())
+                {
+                    if (ProductExists(NumeroProduto, database))
+                    {
+                        // Find the product object that matches the parameters passed
+                        ProdutoEstoque matchingProduct = database.ProdutoEstoque.First(p =>
+                            string.Compare(p.NomeProduto, NumeroProduto) == 0);
+
+                        produto = new Produto()
+                        {
+                            NumeroProduto = matchingProduct.NumeroProduto,
+                            NomeProduto = matchingProduct.NomeProduto,
+                            DescricaoProduto = matchingProduct.DescricaoProduto,
+                            EstoqueProduto = matchingProduct.EstoqueProduto
+                        };
+                    }
+                }
+            }
+            catch
+            {
+                // Ignore exceptions in this implementation
+            }
+            // Return the product
+            return produto;
+        }
+
+        public bool ProductExists(string NumeroProduto, ProvedorEstoque database)
+        {
+            // Check to see whether the specified product exists in the database
+            int numProducts = (from p in database.ProdutoEstoque
+                               where string.Equals(p.NumeroProduto, NumeroProduto)
+                               select p).Count();
+
+            return numProducts > 0;
+        }
+    }
+}
